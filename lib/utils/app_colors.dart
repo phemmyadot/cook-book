88429f854b7bcb9af525eb999cedce07fded5ae9
Babafemi Adojutelegan <badojutelegan@eminenttechnology.com>
2021-06-02 17:42:33 +0100

    add recipe screen

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color primary = Colors.green[800];
+  static Color darkPrimary = Colors.green[900];
   static Color white = Colors.white;
   static Color bg2 = Colors.white70;
   static Color bg1 = Colors.grey[200];
--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -41,49 +41,51 @@ class _CustomAppBarState extends State<CustomAppBar> {
       title: Container(
         padding: EdgeInsets.symmetric(horizontal: 30.0),
         height: 40.0,
-        child: TextField(
-          key: _searchKey,
-          textCapitalization: TextCapitalization.sentences,
-          controller: _searchController,
-          style:
-              TextStyle(fontSize: 16.0, height: 1.0, color: AppColors.inactive),
-          decoration: new InputDecoration(
-            border: new OutlineInputBorder(
-              borderSide: BorderSide(color: AppColors.primary),
-              borderRadius: const BorderRadius.all(
-                const Radius.circular(50.0),
+        child: Form(
+          child: TextField(
+            key: _searchKey,
+            textCapitalization: TextCapitalization.sentences,
+            controller: _searchController,
+            style: TextStyle(
+                fontSize: 16.0, height: 1.0, color: AppColors.inactive),
+            decoration: new InputDecoration(
+              border: new OutlineInputBorder(
+                borderSide: BorderSide(color: AppColors.primary),
+                borderRadius: const BorderRadius.all(
+                  const Radius.circular(50.0),
+                ),
               ),
-            ),
-            enabledBorder: new OutlineInputBorder(
-              borderSide: BorderSide(color: AppColors.primary),
-              borderRadius: const BorderRadius.all(
-                const Radius.circular(50.0),
+              enabledBorder: new OutlineInputBorder(
+                borderSide: BorderSide(color: AppColors.primary),
+                borderRadius: const BorderRadius.all(
+                  const Radius.circular(50.0),
+                ),
               ),
-            ),
-            focusedBorder: new OutlineInputBorder(
-              borderSide: BorderSide(color: AppColors.primary),
-              borderRadius: const BorderRadius.all(
-                const Radius.circular(50.0),
+              focusedBorder: new OutlineInputBorder(
+                borderSide: BorderSide(color: AppColors.primary),
+                borderRadius: const BorderRadius.all(
+                  const Radius.circular(50.0),
+                ),
               ),
-            ),
-            prefixIcon: IconButton(
-              onPressed: () => _searchRecipe(),
-              icon: Icon(
-                Icons.search,
-                size: 20,
-                color: AppColors.primary,
+              prefixIcon: IconButton(
+                onPressed: () => _searchRecipe(),
+                icon: Icon(
+                  Icons.search,
+                  size: 20,
+                  color: AppColors.primary,
+                ),
               ),
+              focusColor: AppColors.primary,
+              hoverColor: AppColors.primary,
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+              isDense: true,
+              filled: true,
+              hintStyle: new TextStyle(color: AppColors.inactive, height: 1.0),
+              hintText:
+                  "Search ${widget.isFavorite ? 'from your favorites' : 'from all recipes'}",
+              fillColor: AppColors.bg2,
             ),
-            focusColor: AppColors.primary,
-            hoverColor: AppColors.primary,
-            contentPadding:
-                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
-            isDense: true,
-            filled: true,
-            hintStyle: new TextStyle(color: AppColors.inactive, height: 1.0),
-            hintText:
-                "Search ${widget.isFavorite ? 'from your favorites' : 'from all recipes'}",
-            fillColor: AppColors.bg2,
           ),
         ),
       ),

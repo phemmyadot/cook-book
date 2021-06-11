da24df801baba1c454e422dfd247ffc95ce7b8ec
Babafemi Adojutelegan <badojutelegan@eminenttechnology.com>
2021-06-11 13:46:48 +0100

    token

--- lib/services/app_services.dart
+++ lib/services/app_services.dart
@@ -58,7 +58,7 @@ class AppServices {
   Future<void> updateToken(String token, String userId) async {
     try {
       if (userId != null)
-        await _userCollectionReference.doc(userId).update({'token': token});
+        _userCollectionReference.doc(userId).update({'token': token});
     } catch (e) {
       throw HttpException(e.message);
     }
